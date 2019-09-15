becb8bf7f07169c27c15fee229a80b9ff3ecfd60
V-Radu <vradu777@yahoo.com>
2019-09-15 20:49:23 +0100

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		//Numerated variables
+		byte myByte = 1; //137
+		short myShort =1; //32000
+		int i = 2;
+		long l = 2;
+		float ff = 0.5f;
+		double myD = 1.1; //More precise then a float
+	}
+}
