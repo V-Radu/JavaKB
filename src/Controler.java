9aaa9b308cccd77e883d9218d6ddf0fb094ee7ab
V-Radu <vradu777@yahoo.com>
2019-09-15 21:02:15 +0100

    add controler

---
+++ src/Controler.java
@@ -0,0 +1,7 @@
+
+public class Controler {
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/Variables.java
+++ src/Variables.java
@@ -1,14 +1,24 @@
 
 public class Variables {
 
-	public static void main(String[] args) {
 		
-		//Numerated variables
-		byte myByte = 1; //137
-		short myShort =1; //32000
-		int i = 2;
-		long l = 2;
-		float ff = 0.5f;
-		double myD = 1.1; //More precise then a float
-	}
+	//Numerated variables
+	byte myByte = 1; //137
+	short myShort =1; //32000
+	int i = 2;
+	long l = 2;
+	float ff = 0.5f;
+	double myD = 1.1; //More precise then a float
+	
+	//alphabetical
+	char c= 'a';
+	String msg = "Hello";
+	
+	
+	//conditionals
+	boolean isTrue = false; //or true only
+	
+		
+		
+	
 }
